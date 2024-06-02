54e1d0e24f6195740bb376a96a4af2419e2345cc
Paige <sailcali@gmail.com>
2024-06-02 13:07:27 -0700

    added discord to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,26 @@
 from flask import Flask
 from dotenv import load_dotenv
+import os
+from discordwebhook import Discord
 from .utils.sensors import Sensors
 from .utils.solarvalve import SolarValve
 from .utils.maintainer import Maintainer
 
 load_dotenv()
 
+DISCORD_POOL_URL = os.environ.get("DISCORD_POOL_URL")
+DISCORD = Discord(url=DISCORD_POOL_URL)
+
 sensors = Sensors()
 valve = SolarValve()
 
 def create_app():
     app = Flask(__name__)
-
+    
+    DISCORD.post("Server running")
     maintainer = Maintainer(sensors, valve)
-
     maintainer.start()
+    DISCORD.post("Maintainer running")
 
     from .routes import pool_bp
 
