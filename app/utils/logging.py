d9652d6d1b8281cb2f316ddc85be804a337b995b
Paige <sailcali@gmail.com>
2024-06-02 12:19:35 -0700

    internalized discord notifications

--- app/utils/logging.py
+++ app/utils/logging.py
@@ -1,6 +1,13 @@
-import requests
+# import requests
+from discordwebhook import Discord
+import os
+
+DISCORD_POOL_URL = os.environ.get("DISCORD_POOL_URL")
+DISCORD = Discord(url=DISCORD_POOL_URL)
 
 def logging(string=None):
+        DISCORD.post(content=string)
         
-        result = requests.post("http://192.168.86.205/pool/notification", json={"message":string})
-        result.close()
+        # DEPRECATED
+        # result = requests.post("http://192.168.86.205/pool/notification", json={"message":string})
+        # result.close()
