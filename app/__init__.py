dab6bd331503af46b3aa187d3a718c11e7203080
Paige <sailcali@gmail.com>
2024-06-02 17:35:22 -0700

    env variables working

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,9 @@
 from flask import Flask
-# from dotenv import load_dotenv
 import os
 from discordwebhook import Discord
 from .utils.sensors import Sensors
 from .utils.solarvalve import SolarValve
 from .utils.maintainer import Maintainer
-
-# load_dotenv()
 
 DISCORD_POOL_URL = os.environ.get("DISCORD_POOL_URL")
 DISCORD = Discord(url=DISCORD_POOL_URL)
@@ -16,10 +13,9 @@ valve = SolarValve()
 
 def create_app():
     app = Flask(__name__)
-    print(DISCORD_POOL_URL)
     DISCORD.post(content="Server running")
-    # maintainer = Maintainer(sensors, valve)
-    # maintainer.start()
+    maintainer = Maintainer(sensors, valve)
+    maintainer.start()
     DISCORD.post(content="Maintainer running")
 
     from .routes import pool_bp
